4175c4c9407fcc7205d3fa03aba579f5123c17d2
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-05 00:44:24 -0800

    Update testt.py

--- testt.py
+++ testt.py
@@ -1,20 +1,13 @@
 import streamlit as st
 
 st.markdown("# Welcome!")
-def main_page():
-    st.snow() 
-    
-    st.markdown("# Welcome to the Rent Predictor 🏘🎄")
-    st.markdown("### In the sidebar to the left there are several pages that can take you through the machine learning side of the predictor.")
-    st.markdown("### If you wish to go straight to the predictor select that page.")
-    from PIL import Image 
-    image1 = Image.open('/Users/austinpullar/Desktop/house3.jpeg')
-    st.image(image1)
 
-    st.markdown("# ENJOY!")
-    st.sidebar.markdown("# Welcome!🎈❄️")
+st.snow() 
     
-    page_names_to_funcs = {
-    "Welcome Page": main_page,
-    }
+st.markdown("# Welcome to the Rent Predictor 🏘🎄")
+
+st.markdown("# ENJOY!")
+st.sidebar.markdown("# Welcome!🎈❄️")
+    
+
 
