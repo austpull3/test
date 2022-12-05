595df9345b2bcbc80d7107521acdef89bfc9644a
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-05 01:00:53 -0800

    Update testt.py

--- testt.py
+++ testt.py
@@ -10,7 +10,7 @@ st.markdown("# ENJOY!")
 st.sidebar.markdown("# Welcome!🎈❄️")
 
 import base64
-    def add_bg_from_local(image_file):
+def add_bg_from_local(image_file):
         with open(image_file, "rb") as image_file:
             encoded_string = base64.b64encode(image_file.read())
         st.markdown(
