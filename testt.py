50544c050d70bec1c1bcb7cd519702baa660015d
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-05 01:03:01 -0800

    Update testt.py

--- testt.py
+++ testt.py
@@ -9,21 +9,9 @@ st.markdown("# Welcome to the Rent Predictor 🏘🎄")
 st.markdown("# ENJOY!")
 st.sidebar.markdown("# Welcome!🎈❄️")
 
-import base64
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-         encoded_string = base64.b64encode(image_file.read())
-         st.markdown(
-         f"""
-         <style>
-         .stApp {{
-         background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-         background-size: cover
-         }}
-         </style>
-          """,
-          unsafe_allow_html=True
-            )
-        add_bg_from_local('anational.webp')   
+from PIL import Image 
+image1 = Image.open('anational.webp')
+st.image(image1)
+
 
 
