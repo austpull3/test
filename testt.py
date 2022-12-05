0b6834798cb560fb84544db4ec49b07674fcdbb2
austpull3 <119237117+austpull3@users.noreply.github.com>
2022-12-05 01:01:54 -0800

    Update testt.py

--- testt.py
+++ testt.py
@@ -11,18 +11,18 @@ st.sidebar.markdown("# Welcome!🎈❄️")
 
 import base64
 def add_bg_from_local(image_file):
-        with open(image_file, "rb") as image_file:
-            encoded_string = base64.b64encode(image_file.read())
-        st.markdown(
-        f"""
-        <style>
-        .stApp {{
-            background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-            background-size: cover
-        }}
-        </style>
-        """,
-        unsafe_allow_html=True
+with open(image_file, "rb") as image_file:
+     encoded_string = base64.b64encode(image_file.read())
+     st.markdown(
+     f"""
+     <style>
+     .stApp {{
+     background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+     background-size: cover
+     }}
+     </style>
+      """,
+      unsafe_allow_html=True
         )
     add_bg_from_local('anational.webp')   
 
